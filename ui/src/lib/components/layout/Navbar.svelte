d5872884d2be93e7e957f751c48d78632b147d5d
Ethan R <1287048315@qq.com>
2024-12-28 22:36:18 +0800

    Add UI code using Svelte

---
+++ ui/src/lib/components/layout/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import Icon from "@iconify/svelte";
+import Autocomplete from "@smui-extra/autocomplete";
+
+let model = ['OPENAPI', 'OLLAMA'];
+
+let valueOutlined: string | undefined = $state();
+</script>
+
+<div>
+    <div>
+        <button>
+            <Icon icon="meteor-icons:xmark" width="24" height="24" />
+        </button>
+    </div>
+    <div>
+        <div>
+            <Autocomplete
+              options={model}
+              textfield$variant="outlined"
+              bind:value={valueOutlined}
+              label="Outlined"
+            />
+        
+            <pre class="status">Selected: {valueOutlined || ''}</pre>
+          </div>
+    </div>
+</div>
---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+import '../app.css';
+import Navbar from '$lib/components/layout/Navbar.svelte';
+let isDark: Boolean = true; 
+
+  const toggleTheme = (): void => {
+    const link = document.getElementById('theme-style') as HTMLLinkElement | null;
+
+    if (link) {
+      let currentTheme = isDark ? '-dark' : '';
+      link.href = `smui${currentTheme}.css`;
+    } else {
+      console.error('Theme link element not found');
+    }
+	isDark = !isDark;
+  };
+  
+
+  
+
+</script>
+
+<svelte:head>
+	<title>Knowledge-GPT</title>
+	<link id="theme-style" rel="stylesheet" type="text/css" href="/smui.css" />
+</svelte:head>
+
+<div class="container">
+	<section class="left">
+		<button on:click={toggleTheme}>
+			Toggle Theme
+		  </button>
+		<Navbar/>
+	</section>
+	<section class="right">
+		<slot/>
+	</section>
+</div>
+
+
+
+<style>
+	.container {
+		display: flex;
+		height: 100%;
+	}
+	.left {
+		width: 30%;
+		float: left;
+		border: 1px solid #000;
+		height: 100%;
+	}
+	.right {
+		width: 70%;
+		float: right;
+	}
+</style>
