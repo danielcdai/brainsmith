49c4609fe49130262398d737c4dc61c62da65b86
Daniel Dai <dch950429@gmail.com>
2025-01-01 17:24:22 +0800

    Unit test for searching.

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -7,10 +7,14 @@ class Settings(BaseSettings):
     log_level: str = "INFO"
     log_dir: str = "logs"
     static_dist_path: str = "ui/build"
-    embeddings_dir: str = "embedding_results"
+    embeddings_dir: str = "./embedding_results"
 
-    class Config:
+    model_config = SettingsConfigDict(
         # TODO: Load the env file path from an environment variable.
-        env_file = ".env"
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="",
+        extra="allow",
+    )
 
 settings = Settings()
---
+++ tests/test_embedding.py
@@ -0,0 +1,53 @@
+import pytest
+import os
+import shutil
+from app.config import settings
+from app.retrieval.search import search_by_collection
+import random
+from app.retrieval.embedding import (
+    start_embedding_task,
+    initialize_embedding_task
+)
+
+
+@pytest.fixture
+def setup_embedding_task():
+    task_id = "test_task"
+    name = "test_embedding"
+    texts = [
+        "Alice and Bob are siblings.",
+        "Bob and Charlie are best friends.",
+        "Charlie and Dave work together.",
+        "Dave and Eve are neighbors.",
+        "Eve and Alice went to the same school.",
+        "Alice and Charlie are cousins.",
+        "Bob and Dave play in the same football team.",
+        "Charlie and Eve are in the same book club.",
+        "Dave and Alice volunteer at the same charity.",
+        "Eve and Bob are gym buddies."
+    ]
+    initialize_embedding_task(task_id)
+    yield task_id, name, texts
+
+    # Cleanup
+    if os.path.exists(settings.embeddings_dir):
+        shutil.rmtree(settings.embeddings_dir)
+
+
+def test_start_embedding_task_and_search(setup_embedding_task):
+    task_id, name, texts = setup_embedding_task
+    start_embedding_task(name, task_id, texts)
+    questions = [
+        "Who are siblings?",
+        "Who are best friends?",
+        "Who works together?",
+        "Who are neighbors?",
+        "Who went to the same school?"
+    ]
+    random_question = random.choice(questions)
+    results = search_by_collection(collection_name=name, query=random_question, top_k=3, search_type="similarity")
+    mmr_results = search_by_collection(collection_name=name, query=random_question, top_k=3, search_type="mmr", fetch_k=10)
+    assert len(results) == 3
+    assert len(mmr_results) == 3
+    assert results != mmr_results
+
